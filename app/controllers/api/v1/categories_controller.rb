67d5b5d84d4b3f70b340f79d36809cfff5c7d8de
seeduardo <romenflatiron@gmail.com>
2018-12-28 17:49:22 +0000

    added json error message to categories_controller; temp allowed all domains in cors.rb

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::CategoriesController < ApplicationController
-    
+
     def index
       @categories = Category.all
       render json: @categories
     end
 
     def show
-      @category = Category.find(params[:id])
-      render json: @category
+      @category = Category.find_by id: (params[:id])
+      if @category
+        render json: @category
+      else
+        render json: {error: 'Category not found.'}, status: 404
+      end
     end
 
 end
