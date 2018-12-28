6fc19784fe4dd6c990e31e1299439a577d250a38
seeduardo <romenflatiron@gmail.com>
2018-12-28 18:19:42 +0000

    added greeting_cards to category_serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,10 +1,12 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
 
-# class GreetingCardSerializer < ActiveModel::Serializer
-#   attributes :id
+  attributes :id, :name, :greeting_cards
+  has_many :greeting_cards
 
-# end
+  class GreetingCardSerializer < ActiveModel::Serializer
+    attributes :id, :title, :description, :image
+
+  end
 
 
 end
